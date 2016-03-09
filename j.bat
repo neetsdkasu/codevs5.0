b44b785c81129b6f1afbe0cc710daa34cd65bfef
Leonardone <neetsdkasu@yahoo.co.uk>
2016-03-10 02:02:09 +0900

    何か変だったので修正した

--- j.bat
+++ j.bat
@@ -1,10 +1,10 @@
 @if exist ManualPlay.jar goto update_label
 
-jar cvf ManualPlay.jar -C classes/ .
+jar cvf ManualPlay.jar -C classes .
 
 @exit /b
 
 :update_label
 
-jar uvf ManualPlay.jar -C classes/ .
+jar uvf ManualPlay.jar -C classes .
 
